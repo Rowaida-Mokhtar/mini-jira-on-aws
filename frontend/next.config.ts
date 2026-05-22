2ba43e5170c3d26b7db14ca697acef28b0beff5e
rana abouraia <16abourr@gmail.com>
2026-05-22 20:45:29 +0300

    Adding pages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    const apiUrl =
+      process.env.NEXT_PUBLIC_API_URL || "https://d2abi2mqrclj5d.cloudfront.net";
+
+    return [
+      {
+        source: "/api/backend/:path*",
+        destination: `${apiUrl}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
